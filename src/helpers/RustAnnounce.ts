cb93a9d26dd352d923ff3c5e085a511577f0f386
egee-irl <brian@egee.io>
2018-11-13 03:02:02 -0800

    rust shouldnt log everything

--- src/helpers/RustAnnounce.ts
+++ src/helpers/RustAnnounce.ts
@@ -5,24 +5,23 @@ export default function RustAnnounce(
   guild: any,
   message: string
 ) {
-  if (message.includes("joined [")) {
-    const playerName = message.match(/([^/]*)\s*\s/);
-    const playerNameNormalized = playerName.pop().trim();
-    const discordAnnoucement = playerNameNormalized.replace(
-      "joined",
-      "logged in to"
-    );
-    const serverAnnoucement = playerName.pop().trim() + " the server";
-    rustClient.run("say " + serverAnnoucement);
-
-    SendChannelMessage(
-      guild,
-      "rust-server",
-      `${discordAnnoucement} the server`
-    ).catch(err => {
-      console.error(`Error announcing: ${err}`);
-    });
-  } else if (message.includes("Saving") === false) {
-    console.log(message + " - " + Date.now());
+  switch (true) {
+    case message.includes("joined ["):
+      const playerName = message.match(/([^/]*)\s*\s/);
+      const playerNameNormalized = playerName.pop().trim();
+      const discordAnnoucement = playerNameNormalized.replace(
+        "joined",
+        "logged in to"
+      );
+      const serverAnnoucement = playerName.pop().trim() + " the server";
+      rustClient.run("say " + serverAnnoucement);
+      SendChannelMessage(
+        guild,
+        "rust-server",
+        `${discordAnnoucement} the server`
+      ).catch(err => {
+        console.error(`Error announcing: ${err}`);
+      });
+      break;
   }
 }
