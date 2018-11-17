fc6e4b2ca7f2399075191ddd2ab7a6e37673256e
egee-irl <brian@egee.io>
2018-11-17 02:41:54 -0800

    docker log poc

--- src/listeners/Minecraft.ts
+++ src/listeners/Minecraft.ts
@@ -1,4 +1,7 @@
 import Connect from "../Connect";
+var stream = require("stream");
+import Docker from "dockerode";
+import GetContainer from "../commands/GetContainer";
 import { Guild } from "discord.js";
 import SendChannelMessage from "../helpers/SendChannelMessage";
 
@@ -8,8 +11,54 @@ export default class MinecraftListener {
   constructor(Guild: Guild) {
     this.guild = Guild;
     this.playerList = [];
-    setInterval(this.ListPlayers, 10000, this);
+    const engine = new Docker({ host: process.env.HOST, port: 2376 });
+    this.test("gscminecraft_minecraft-server_1", engine);
+    // setInterval(this.ListPlayers, 60000, this);
   }
+  async test(containerName: string, engine: Docker) {
+    const container = await GetContainer(containerName, engine);
+    const ts = Math.floor(+new Date() / 1000 - 6000);
+    this.containerLogs(container, ts);
+
+    // const logs = await container.logs({ since: ts, stdout: true });
+    // let blank = "" + logs;
+    // blank = blank.trim();
+    // console.log(blank);
+  }
+
+  containerLogs(container, ts) {
+    // create a single stream for stdin and stdout
+    const logStream = new stream.PassThrough();
+    let myArray = [];
+    logStream.on("data", function(chunk) {
+      let blank = chunk.toString("utf8");
+      myArray.push(blank);
+      console.log(blank);
+    });
+
+    container.logs(
+      {
+        since: ts,
+        follow: true,
+        stdout: true,
+        stderr: true
+      },
+      (err, stream) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        container.modem.demuxStream(stream, logStream, logStream);
+        stream.on("end", () => {
+          logStream.end();
+        });
+
+        setTimeout(() => {
+          stream.destroy();
+        }, 2000);
+      }
+    );
+  }
+
   async ListPlayers(self) {
     let intersection = [];
     let thePlayers = "";
