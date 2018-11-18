2489de95c59180426bdeae18cbc4a932a607f2f6
egee-irl <brian@egee.io>
2018-11-18 02:19:46 -0800

    replaced class with function

--- src/Listen.ts
+++ src/Listen.ts
@@ -1,15 +1,17 @@
+import Docker from "dockerode";
 import Connect from "./Connect";
 import RustListener from "./listeners/Rust";
 import DiscordListener from "./listeners/Discord";
 import MinecraftListener from "./listeners/Minecraft";
 
 export default async function Listen() {
+  const engine = new Docker({ host: process.env.HOST, port: 2376 });
   const discordClient = await Connect.Discord();
   const rustClient = await Connect.Rust();
   try {
     RustListener(rustClient, discordClient.guilds.first());
     DiscordListener(discordClient);
-    new MinecraftListener(discordClient.guilds.first());
+    setInterval(MinecraftListener, 30000, discordClient.guilds.first(), engine);
   } catch (err) {
     console.error(
       `Something went terribly wrong while setting up the Listeners: ${err}`
--- src/listeners/Minecraft.ts
+++ src/listeners/Minecraft.ts
@@ -1,66 +1,58 @@
-import Connect from "../Connect";
-var stream = require("stream");
+import stream from "stream";
 import Docker from "dockerode";
 import GetContainer from "../commands/GetContainer";
 import { Guild } from "discord.js";
 import SendChannelMessage from "../helpers/SendChannelMessage";
 
-export default class MinecraftListener {
-  playerList: Array<string>;
-  guild: Guild;
-  containerName: String;
-  engine: Docker;
-  constructor(Guild: Guild) {
-    this.guild = Guild;
-    this.playerList = [];
-    this.engine = new Docker({ host: process.env.HOST, port: 2376 });
-    this.containerName = "gscminecraft_minecraft-server_1";
-    setInterval(this.test, 30000, this);
-  }
-  async test(self) {
-    const container = await GetContainer(self.containerName, self.engine);
-    const ts = Math.floor(+new Date() / 1000 - 30);
-    self.containerLogs(container, ts, self);
-  }
+export default async function MinecraftListener(
+  discordGuild: Guild,
+  engine: Docker
+) {
+  const container = await GetContainer(
+    "gscminecraft_minecraft-server_1",
+    engine
+  );
+  const unixTimeStamp = Math.floor(+new Date() / 1000 - 30);
+  containerLogs(container, unixTimeStamp, discordGuild);
+}
 
-  containerLogs(container, ts, self) {
-    // create a single stream for stdin and stdout
-    const logStream = new stream.PassThrough();
-    logStream.on("data", function(chunk) {
-      const blank = chunk.toString("utf8");
-      const myregex = blank.match(/(?<=\bUUID\sof\splayer\s)(\w+)/);
-      if (myregex !== null) {
-        const thePlayer = `\`${myregex[0]}\` joined the server`;
-        SendChannelMessage(self.guild, "debug", thePlayer)
-          .then(() => {
-            console.info(thePlayer);
-          })
-          .catch(err => {
-            console.log(`The minecraft loop failed: ${err}`);
-          });
-      }
-    });
-
-    container.logs(
-      {
-        since: ts,
-        follow: true,
-        stdout: true,
-        stderr: true
-      },
-      (err, stream) => {
-        if (err) {
-          return console.error(err.message);
-        }
-        container.modem.demuxStream(stream, logStream, logStream);
-        stream.on("end", () => {
-          logStream.end();
+function containerLogs(container, ts, discordGuild: Guild) {
+  // create a single stream for stdin and stdout
+  const logStream = new stream.PassThrough();
+  logStream.on("data", function(chunk) {
+    const blank = chunk.toString("utf8");
+    const myregex = blank.match(/(?<=\bUUID\sof\splayer\s)(\w+)/);
+    if (myregex !== null) {
+      const thePlayer = `\`${myregex[0]}\` joined the server`;
+      SendChannelMessage(discordGuild, "debug", thePlayer)
+        .then(() => {
+          console.info(thePlayer);
+        })
+        .catch(err => {
+          console.log(`The minecraft loop failed: ${err}`);
         });
+    }
+  });
 
-        setTimeout(() => {
-          stream.destroy();
-        }, 2000);
+  container.logs(
+    {
+      since: ts,
+      follow: true,
+      stdout: true,
+      stderr: true
+    },
+    (err, stream) => {
+      if (err) {
+        return console.error(err.message);
       }
-    );
-  }
+      container.modem.demuxStream(stream, logStream, logStream);
+      stream.on("end", () => {
+        logStream.end();
+      });
+
+      setTimeout(() => {
+        stream.destroy();
+      }, 2000);
+    }
+  );
 }
