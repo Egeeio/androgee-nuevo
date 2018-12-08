320c9b23f27986a913e0305e201e94a7a7358034
egee-irl <brian@egee.io>
2018-12-08 04:08:29 -0800

    fixed logging for sdtd

--- src/helpers/GenericGameAnnounce.ts
+++ src/helpers/GenericGameAnnounce.ts
@@ -1,4 +1,3 @@
-import stream from "stream";
 import { Guild } from "discord.js";
 import SendChannelMessage from "./SendChannelMessage";
 import GetLastChannelMessage from "./GetLastChannelMessage";
@@ -10,45 +9,21 @@ export default async function GenericGameAnnounce(
   channel: string,
   discordGuild: Guild
 ) {
-  const logStream = new stream.PassThrough();
   const lastMsg = await GetLastChannelMessage(discordGuild, channel);
-  logStream.on("data", chunk => {
-    const line = chunk.toString("utf8");
-    const regexMatch = line.match(regex);
-    if (regexMatch !== null) {
-      const thePlayer = `**${regexMatch[0]}** joined the server`;
-      const debug = lastMsg !== thePlayer && line.includes("joined");
-      if (lastMsg !== thePlayer) {
-        SendChannelMessage(discordGuild, channel, thePlayer)
-          .then(() => {
-            console.info(thePlayer);
-          })
-          .catch(err => {
-            console.error(`The minecraft loop failed: ${err}`);
-          });
-      }
-    }
-  });
+  const logs = await container.logs({ since: timeStamp, stdout: true });
 
-  container.logs(
-    {
-      since: timeStamp,
-      follow: true,
-      stdout: true,
-      stderr: true
-    },
-    (err, stream) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      container.modem.demuxStream(stream, logStream, logStream);
-      stream.on("end", () => {
-        logStream.end();
-      });
-
-      setTimeout(() => {
-        stream.destroy();
-      }, 2000);
+  const regexMatch = logs.match(regex);
+  if (regexMatch !== null) {
+    const thePlayer = `**${regexMatch[0]}** joined the server`;
+    const debug = lastMsg !== thePlayer && logs.includes("joined");
+    if (lastMsg !== thePlayer) {
+      SendChannelMessage(discordGuild, channel, thePlayer)
+        .then(() => {
+          console.info(thePlayer);
+        })
+        .catch(err => {
+          console.error(`The minecraft loop failed: ${err}`);
+        });
     }
-  );
+  }
 }
